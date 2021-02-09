d21234adf4bda467230ce1c9d536d82422abb9f6
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2021-02-08 22:15:16 -0500

    Update logic.js

--- actualApp/static/js/logic.js
+++ actualApp/static/js/logic.js
@@ -1,7 +1,7 @@
 // Create the tile layer that will be the background of our map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  maxZoom: 15,
+  maxZoom: 20,
   id: "mapbox/satellite-v9",
   accessToken: API_KEY
 });
