0efdcb7017b6e6148013335dff6b33f2bdcd9978
Caitlan Beachey <71155961+cbeachey@users.noreply.github.com>
2021-02-15 12:00:01 -0500

    UPDATE

--- actualApp/static/js/app.js
+++ actualApp/static/js/app.js
@@ -1,3 +1,21 @@
 // var url =  d3.json("/api/borough").then(function(data){
 // console.log(data)
 // })
+
+
+
+
+var $grid = $('.grid').masonry({
+    columnWidth: 120,
+    itemSelector: '.grid-item'
+  });
+  
+  $grid.on( 'click', '.grid-item-content', function( event ) {
+    $( event.currentTarget ).parent('.grid-item').toggleClass('is-expanded');
+    $grid.masonry();
+  });
+  
+
+
+
+
